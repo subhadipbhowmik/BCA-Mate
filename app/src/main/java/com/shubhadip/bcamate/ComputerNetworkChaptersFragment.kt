15741660cb9bef5e22f29c2420313eee39589bb8
subhadipbhowmik <shubhadipbhowmik02@gmail.com>
2023-08-10 00:17:26 +0530

    tab testing

--- app/src/main/java/com/shubhadip/bcamate/ComputerNetworkChaptersFragment.kt
+++ app/src/main/java/com/shubhadip/bcamate/ComputerNetworkChaptersFragment.kt
@@ -1,19 +1,36 @@
 package com.shubhadip.bcamate
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import com.shubhadip.bcamate.databinding.FragmentComputerNetworkChaptersBinding
 
 class ComputerNetworkChaptersFragment : Fragment() {
-
+    private  var _binding: FragmentComputerNetworkChaptersBinding? = null
+    private val binding get() = _binding!!
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_computer_network_chapters, container, false)
+        _binding = FragmentComputerNetworkChaptersBinding.inflate(inflater, container, false)
+
+      //   unit_01_topic_01
+
+        binding.cnTopic111Btn.setOnClickListener {
+            Toast.makeText(requireActivity(), "Opening PDF", Toast.LENGTH_SHORT).show()
+
+            var driveUrl = "https://drive.google.com/file/d/1-1TRpIJXN4qOTFPV-Xsokx0RE-4hj6Va/view?usp=drive_link"
+            startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(driveUrl)))
+        }
+
+
+        return  binding.root
     }
 
 }
