97dff71df694d236a5174e83647f0987e717f473
subhadipbhowmik <shubhadipbhowmik02@gmail.com>
2023-08-20 23:12:58 +0530

    updated ui

--- app/src/main/java/com/shubhadip/bcamate/fragments/Bca22Sec1TimeTable.kt
+++ app/src/main/java/com/shubhadip/bcamate/fragments/Bca22Sec1TimeTable.kt
@@ -1,26 +1,25 @@
-package com.shubhadip.bcamate
+package com.shubhadip.bcamate.fragments
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.shubhadip.bcamate.databinding.FragmentTimeTableBinding
+import com.shubhadip.bcamate.databinding.FragmentBca22Sec1TimeTableBinding
 
-
-class TimeTableFragment : Fragment() {
-    private  var _binding: FragmentTimeTableBinding? = null
+class Bca22Sec1TimeTable : Fragment() {
+    private  var _binding: FragmentBca22Sec1TimeTableBinding? = null
     private val binding get() = _binding!!
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        _binding = FragmentTimeTableBinding.inflate(inflater, container, false)
+        _binding = FragmentBca22Sec1TimeTableBinding.inflate(inflater, container, false)
 
-      
+
         return  binding.root
     }
-
 
 }
